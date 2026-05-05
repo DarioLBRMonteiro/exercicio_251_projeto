e823b5d0bebfb0090fdbd12db439ed1dba3e3c7c
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-04 22:31:43 -0300

    entidade classe departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,11 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.entities.Department;
 
 public class Program {
 	public static void main(String[] args) {
-		//public static void main(String[] args) throws SQLException {
-		//String url = "jdbc:sqlserver://localhost:2866;databaseName=coursejdbc;encrypt=true;trustServerCertificate=true;";
-//		String url = "jdbc:sqlserver://DESKTOP-UILKHHR:2866;databaseName=coursejdbc;encrypt=true;trustServerCertificate=true;";
-//        Connection con = DriverManager.getConnection(url, "sa", "sql@2019");
-//        System.out.println("Conectado: " + con.isValid(1));
-	
-		Connection conn = null;
-		Statement st = null;
-		
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
 		
 				
 	}	
